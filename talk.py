2ac518865d16f924d91b40662f8c58b55ba14bea
BBerry <BBerry.NZ@gmail.com>
2018-04-29 16:00:15 +1200

    Added more comments and functions, made code neat. reading convo from a file as well, prototype 2

--- talk.py
+++ talk.py
@@ -10,47 +10,41 @@ from chatterbot.trainers import ListTrainer
 
 class Talk:
 
-    #Later on we can read the converstaion from a file rather than storing in a function, returns a list of the convo
-    def listofConversation(self):
-        """
-        #Here you can read the conversationList from a file and store in a list rather than putting in function.
-        #For example put the following in the file:
-            "Hi",                                  #Question 1
-            "Hey",                                  #Answer 1
-            "Hello",                                #Question 2
-            "Hi there!",                            #Answer 2
-            "How are you doing?",                   #Question 3
-            "I'm doing great.",                     #Answer 3 and so on....
-            "That is good to hear",
-            "Thank you.",
-            "You're welcome."
-        covoList = list()
-        with open("conversation.txt", "r") as q:
-            convoList.append(q)
-        return convoList
-        """
-        # Using this we can train the chatterbot, this is a list of a converstaion
-        conversation = [
-            "Hi",
-            "Hey",
-            "Hello",
-            "Hi there!",
-            "How are you doing?",
-            "I'm doing great.",
-            "That is good to hear",
-            "Thank you.",
-            "You're welcome."
-        ]
-        return conversation
+    # This is a constructor, not necessary at this stage but may need it later.
+    #def __init__(self):
 
-    def convo(self, chatterbot):
+
+    """
+    This function gets a list of the conversation from a file and stores it in a list.
+    To train the bot even more add more question answers to the list in same format, right now the conversation list is just an example, modify according to your own needs
+    returns conversationList
+    """
+    def getConversations(self):
+        conversationList = list
+        with open("conversations.txt", "r") as convo:
+            # Reads from the file and store the conversation in a list, removes new line characters (\n)
+            conversationList = convo.read().splitlines()
+            # Print statement here for debugging purposes, can be removed later if you want.
+            print(conversationList)
+        return conversationList
+
+    """
+    This function keeps on looping until the user enters q to quit, user input is asked and answer is given depending on the user input
+    returns the userInput
+    """
+    def startTalking(self, chatterbot):
         while True:
-            # User input is asked
-            userInput = input("Ask Skynet the bot a question or q to quit: ")
-            # gets the response from the bot according to the userInput given
-            response = chatterbot.get_response(userInput)
+            # Trying to catch any unhandled exceptions, for now will just break out and leave it
+            try:
+                # User input is asked
+                userInput = input("Ask Skynet the bot a question or q to quit: ")
+                # Gets a response from the bot according to the userInput given
+                response = chatterbot.get_response(userInput)
+            except (KeyboardInterrupt, EOFError, SystemExit):
+                break
+
             # Quits if user enters q
-            if userInput == 'q':
+            if userInput == 'q' or userInput == 'Q':
                 break
             # Prints the response.
             print(response)
@@ -59,19 +53,23 @@ class Talk:
 
 def main():
     #Creats a ChatBot object, the bot is named Skynet
+    #This will help solve math problems and give current time etc...... But then stops having proper conversation, need to fix later
+    #chatterbot = ChatBot("Skynet", logic_adapters=["chatterbot.logic.MathematicalEvaluation","chatterbot.logic.TimeLogicAdapter"])
     chatterbot = ChatBot("Skynet")
-
-    #Creates a object Talk object
+    #Creates a Talk object
     talk = Talk()
 
-    conversation = talk.listofConversation()
+    #Gets the list of conversations
+    conversations = talk.getConversations()
 
     # The bot gets trained
     chatterbot.set_trainer(ListTrainer)
 
-    # The bot can have a simple Conversation after being trained with the list of Conversation
-    chatterbot.train(conversation)
-    talk.convo(chatterbot)
+    # The bot is trained by taking a list of statements that represent a conversation
+    chatterbot.train(conversations)
+
+    #The conversation begins
+    talk.startTalking(chatterbot)
 
 
 if __name__ == '__main__':
