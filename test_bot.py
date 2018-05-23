12d0dc49d1645971b96d4ad15ddfac378e7f3665
dxble <31087795+dxble@users.noreply.github.com>
2018-05-24 00:54:21 +1200

    Pre-requisite User Story done
    Completed for BCIS

--- test_bot.py
+++ test_bot.py
@@ -20,6 +20,10 @@ class TestBotClass(unittest.TestCase):
         userQuestion = self.bot.chatterbot.get_response("Which papers are suitable for Software Development major?")
         self.assertEqual(userQuestion, "COMP603 Program Design & Construction, COMP602 Software Development Practice, COMP604 Operating Systems, OR INFS602 Physical Database Design, Any 3 of the following: ENSE701 Software Engineering, COMP719 Applied Human Computer Interaction, COMP721 Web Development, COMP713 Distributed & Mobile Systems")
 
+    def test_bot_response(self):
+        userQuestion = self.bot.chatterbot.get_response("What are the prerequisites for the Computer Graphics Paper?")
+        self.assertEqual(userQuestion, "Pre-requisite: MATH502 Algebra and Discrete Mathematics, COMP603 Program Design and Construction or COMP610 Data Structures and Algorithms")
+
 # Runs all the unit-tests
 if __name__ == '__main__':
     unittest.main()
